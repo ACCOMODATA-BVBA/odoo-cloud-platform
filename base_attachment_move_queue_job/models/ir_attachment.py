befad99126ec35a42c9a8c431c3139049f5c1ed4
Jos De Graeve <jos.degraeve@accomodata.be>
2023-09-04 11:04:43 +0200

    [FIX] base_attachment_move_queue_job - Avoid SQL error when checksum=False

--- base_attachment_move_queue_job/models/ir_attachment.py
+++ base_attachment_move_queue_job/models/ir_attachment.py
@@ -1,25 +1,42 @@
 import logging
 
-import psycopg2
-
 from odoo import api, models
-
-_logger = logging.getLogger(__name__)
 
 from odoo.addons.base_attachment_object_storage.models.ir_attachment import \
     clean_fs
 
 from odoo.addons.base.models.ir_attachment import IrAttachment as BaseIrAttachment
 
+_logger = logging.getLogger(__name__)
+
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
 
     @api.model
+    def _fixup_checksum_values(self):
+        """
+        To be called ad-hoc / when needed.
+        The checksum value should always be filled in for attachments.
+        Sometimes we see binary attachments without checksum.  Because the
+        store_fname for filestore attachments is based on the checksum, we
+        can repair the checksum value with the store_fname field.
+
+        """
+        self.env.cr.execute(
+            "UPDATE ir_attachment "
+            "SET checksum=substr(store_fname,4,40) "
+            "WHERE checksum is NULL "
+            "  AND store_fname IS NOT NULL "
+            "  AND store_fname ~ '^[0-9a-f]{2}\/[0-9a-f]{40}$' "  # noqa:W605
+            "  AND type='binary' "
+        )
+
+    @api.model
     def _force_storage_to_object_storage(self, new_cr=False):
         # method is called @installation time of the attachment object storage modules
         # For large file stores, this makes the module installation take forever.
-        # Therefor, we install the module, and migrate attachments afterwards using
+        # Therefor, we install the module, and migrate attachments afterward using
         # queue jobs
         _logger.info(
             'Not executing ir_attachment._force_storage_to_object_storage(), '
@@ -63,6 +80,7 @@ class IrAttachment(models.Model):
         # contain 'res_field'.
         # https://github.com/odoo/odoo/blob/9032617120138848c63b3cfa5d1913c5e5ad76db/odoo/addons/base/ir/ir_attachment.py#L344-L347
         domain = [
+            ('type', '=', 'binary'),
             ('store_fname', '!=', False),
             '!', ('store_fname', '=like', f'{storage}://%'),
             '|',
@@ -84,7 +102,8 @@ class IrAttachment(models.Model):
         for checksum, store_fname in group_data:
             # The transfer jobs get high priority, so they are all ran before
             # a new batch of transfer jobs is created
-            self.with_delay(priority=5)._transfer_to_object_storage(checksum, store_fname)
+            self.with_delay(priority=5)._transfer_to_object_storage(checksum,
+                                                                    store_fname)
         return len(group_data)
 
     def _transfer_to_object_storage(self, checksum, store_fname):
@@ -95,16 +114,22 @@ class IrAttachment(models.Model):
                 checksum
             )
             return "Skipping atts with empty store_fname and checksum %s" % checksum
+        elif not checksum:
+            _logger.warning(
+                'Skipping atts with empty checkup and store_fname %s',
+                store_fname
+            )
+            return "Skipping atts with empty checkup and store_fname %s" % store_fname
+
         # check that no other transaction has locked the row, don't send a file to
         # storage in that case
-        self.env.cr.execute("SELECT id "
-                            "FROM ir_attachment "
-                            "WHERE checksum = %s "
-                            "FOR UPDATE NOWAIT",
-                            (checksum,),
-                            log_exceptions=False)
+        self.env.cr.execute(
+            "SELECT id FROM ir_attachment WHERE checksum = %s AND store_fname = %s "
+            "FOR UPDATE NOWAIT", (checksum, store_fname), log_exceptions=False
+        )
         self.env.clear()
         attachments = self.search([
+            ('type', '=', 'binary'),
             ('checksum', '=', checksum),
             ('store_fname', '=', store_fname),
             '|',
